079352b082858fe687fdebee35c1262f53045dcd
chappa <pallavichappa04@gmail.com>
2023-12-19 19:28:54 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming langusge?",
+            "Where you are watching this video?",
+            "What is your name?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"facebook","Whatsapp","Instagram","Youtube"},
+            {"Pallavi","Deepthi","Sravya","Ramya"}
+
+    };
+
+    public static String correctAnswers[] ={
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Pallavi"
+
+    };
+}
